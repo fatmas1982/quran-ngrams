41a9dfb2736dee5b472cd5f1f1ee19ba5d4a64bd
Saqib <docbook.xml@gmail.com>
2016-06-05 17:44:54 +0000

    Fixing the buttons

--- target/scala-2.11/twirl/main/views/html/main.template.scala
+++ target/scala-2.11/twirl/main/views/html/main.template.scala
@@ -27,16 +27,40 @@ Seq[Any](format.raw/*1.32*/("""
 """),format.raw/*3.1*/("""<!DOCTYPE html>
 
 <html>
-    <head>
-        <title>"""),_display_(/*7.17*/title),format.raw/*7.22*/("""</title>
-        <link rel="stylesheet" type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" />
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-        <script type="text/javascript" src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
-        <link rel="stylesheet" type="text/css" href=""""),_display_(/*11.55*/routes/*11.61*/.Assets.at("stylesheets/main.css")),format.raw/*11.95*/("""" />
-    </head>
-    <body>
-        """),_display_(/*14.10*/content),format.raw/*14.17*/("""
-    """),format.raw/*15.5*/("""</body>
+
+<head>
+    <title>"""),_display_(/*8.13*/title),format.raw/*8.18*/("""</title>
+    <link rel="stylesheet" type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" />
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script type="text/javascript" src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+
+
+    <script>
+        (function(i, s, o, g, r, a, m) """),format.raw/*15.40*/("""{"""),format.raw/*15.41*/("""
+            """),format.raw/*16.13*/("""i['GoogleAnalyticsObject'] = r;
+            i[r] = i[r] || function() """),format.raw/*17.39*/("""{"""),format.raw/*17.40*/("""
+                """),format.raw/*18.17*/("""(i[r].q = i[r].q || []).push(arguments)
+            """),format.raw/*19.13*/("""}"""),format.raw/*19.14*/(""", i[r].l = 1 * new Date();
+            a = s.createElement(o),
+                m = s.getElementsByTagName(o)[0];
+            a.async = 1;
+            a.src = g;
+            m.parentNode.insertBefore(a, m)
+        """),format.raw/*25.9*/("""}"""),format.raw/*25.10*/(""")(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+
+        ga('create', 'UA-78811140-1', 'auto');
+        ga('send', 'pageview');
+    </script>
+
+
+
+
+</head>
+
+<body>
+    """),_display_(/*37.6*/content),format.raw/*37.13*/("""
+"""),format.raw/*38.1*/("""</body>
+
 </html>
 """))
       }
@@ -58,11 +82,11 @@ Seq[Any](format.raw/*1.32*/("""
 object main extends main_Scope0.main
               /*
                   -- GENERATED --
-                  DATE: Fri Jun 03 05:36:27 UTC 2016
+                  DATE: Sun Jun 05 17:42:56 UTC 2016
                   SOURCE: /home/ubuntu/workspace/app/views/main.scala.html
-                  HASH: ab306075a8d9faf280fa39e534a41947a1b3eb70
-                  MATRIX: 530->1|655->31|683->33|760->84|785->89|1210->487|1225->493|1280->527|1344->564|1372->571|1404->576
-                  LINES: 20->1|25->1|27->3|31->7|31->7|35->11|35->11|35->11|38->14|38->14|39->15
+                  HASH: 88fa8a9852355596a90328f1f5ab306236aaeaf7
+                  MATRIX: 530->1|655->31|683->33|753->77|778->82|1192->468|1221->469|1262->482|1360->552|1389->553|1434->570|1514->622|1543->623|1783->836|1812->837|2043->1042|2071->1049|2099->1050
+                  LINES: 20->1|25->1|27->3|32->8|32->8|39->15|39->15|40->16|41->17|41->17|42->18|43->19|43->19|49->25|49->25|61->37|61->37|62->38
                   -- GENERATED --
               */
           
--- target/scala-2.11/twirl/main/views/html/search.template.scala
+++ target/scala-2.11/twirl/main/views/html/search.template.scala
@@ -26,38 +26,48 @@ Seq[Any](format.raw/*1.38*/("""
 
 """),_display_(/*3.2*/main("Quran verses containing: " + ngram)/*3.43*/ {_display_(Seq[Any](format.raw/*3.45*/("""
 
-"""),format.raw/*5.1*/("""<div class="jumbotron text-center">
+"""),format.raw/*5.1*/("""<!-- Place this tag where you want the button to render. -->
+<div class="jumbotron text-right">
+<a class="github-button" href="https://github.com/saqib-ali/quran-ngrams" data-style="mega" data-count-href="/saqib-ali/quran-ngrams/stargazers" data-count-api="/repos/saqib-ali/quran-ngrams#stargazers_count" data-count-aria-label="# stargazers on GitHub" aria-label="Star saqib-ali/quran-ngrams on GitHub">Star</a>
+</div>
+
+<div class="jumbotron text-center">
+    
+    
+    
     <div class="container">
 
         <h1>Quran N-gram Analytics</h1>
         <p>This Scala app lets you perform n-gram Analytics on the English Translation of Quran</p>
 
-        <a type="button" class="btn btn-lg btn-default" href="/?numOfWords=10"></span>10-gram</a>
-        <a type="button" class="btn btn-lg btn-default" href="/?numOfWords=11"></span>11-gram</a>
-        <a type="button" class="btn btn-lg btn-default" href="/?numOfWords=12"></span>12-gram</a>
-        <a type="button" class="btn btn-lg btn-default" href="/?numOfWords=13"></span>13-gram</a>
-        <a type="button" class="btn btn-lg btn-default" href="/?numOfWords=14"></span>14-gram</a>
+     
+      <a type="button" class="btn btn-lg btn-default" href="/?numOfWords=1"></span>1 word n-grams</a>
+      <a type="button" class="btn btn-lg btn-default" href="/?numOfWords=10"></span>10 words n-grams</a>
+      <a type="button" class="btn btn-lg btn-default" href="/?numOfWords=11"></span>11 words n-grams</a>
+      <a type="button" class="btn btn-lg btn-default" href="/?numOfWords=12"></span>12 words n-grams</a>
+      <a type="button" class="btn btn-lg btn-default" href="/?numOfWords=13"></span>13 words n-grams</a>
+      <a type="button" class="btn btn-lg btn-default" href="/?numOfWords=14"></span>14 words n-grams</a>
     </div>
 
 </div>
 <div class="container">
 
-    """),_display_(/*21.6*/if(ngram != null)/*21.23*/{_display_(Seq[Any](format.raw/*21.24*/("""
-    """),format.raw/*22.5*/("""<div class="alert alert-success text-center" role="alert">
-        Verses containing: """),_display_(/*23.29*/ngram),format.raw/*23.34*/("""
-    """),format.raw/*24.5*/("""</div>
-    """)))}),format.raw/*25.6*/("""
+    """),_display_(/*31.6*/if(ngram != null)/*31.23*/{_display_(Seq[Any](format.raw/*31.24*/("""
+    """),format.raw/*32.5*/("""<div class="alert alert-success text-center" role="alert">
+        Verses containing: """),_display_(/*33.29*/ngram),format.raw/*33.34*/("""
+    """),format.raw/*34.5*/("""</div>
+    """)))}),format.raw/*35.6*/("""
 
-    """),format.raw/*27.5*/("""<table class="table table-hover">
+    """),format.raw/*37.5*/("""<table class="table table-hover">
         <thead>
         <tr>
             <th>Verse</th>
         </tr>
         </thead>
-        """),_display_(/*33.10*/for((sign) <- signs) yield /*33.30*/ {_display_(Seq[Any](format.raw/*33.32*/("""
-        """),format.raw/*34.9*/("""<tr><td>"""),_display_(/*34.18*/sign),format.raw/*34.22*/("""</td></tr>
-        """)))}),format.raw/*35.10*/("""
-    """),format.raw/*36.5*/("""</table>
+        """),_display_(/*43.10*/for((sign) <- signs) yield /*43.30*/ {_display_(Seq[Any](format.raw/*43.32*/("""
+        """),format.raw/*44.9*/("""<tr><td>"""),_display_(/*44.18*/sign),format.raw/*44.22*/("""</td></tr>
+        """)))}),format.raw/*45.10*/("""
+    """),format.raw/*46.5*/("""</table>
 </div>
 
 """)))}))
@@ -80,11 +90,11 @@ Seq[Any](format.raw/*1.38*/("""
 object search extends search_Scope0.search
               /*
                   -- GENERATED --
-                  DATE: Fri Jun 03 05:04:54 UTC 2016
+                  DATE: Sun Jun 05 17:42:56 UTC 2016
                   SOURCE: /home/ubuntu/workspace/app/views/search.scala.html
-                  HASH: 04b64adeb9b94b28c63af9902dbdb1f85fea3498
-                  MATRIX: 542->1|673->37|701->40|750->81|789->83|817->85|1588->830|1614->847|1653->848|1685->853|1799->940|1825->945|1857->950|1899->962|1932->968|2089->1098|2125->1118|2165->1120|2201->1129|2237->1138|2262->1142|2313->1162|2345->1167
-                  LINES: 20->1|25->1|27->3|27->3|27->3|29->5|45->21|45->21|45->21|46->22|47->23|47->23|48->24|49->25|51->27|57->33|57->33|57->33|58->34|58->34|58->34|59->35|60->36
+                  HASH: 5cc0adfa4077137ba10bf5a8059e0ced817e2e64
+                  MATRIX: 542->1|673->37|701->40|750->81|789->83|817->85|2166->1408|2192->1425|2231->1426|2263->1431|2377->1518|2403->1523|2435->1528|2477->1540|2510->1546|2667->1676|2703->1696|2743->1698|2779->1707|2815->1716|2840->1720|2891->1740|2923->1745
+                  LINES: 20->1|25->1|27->3|27->3|27->3|29->5|55->31|55->31|55->31|56->32|57->33|57->33|58->34|59->35|61->37|67->43|67->43|67->43|68->44|68->44|68->44|69->45|70->46
                   -- GENERATED --
               */
           
