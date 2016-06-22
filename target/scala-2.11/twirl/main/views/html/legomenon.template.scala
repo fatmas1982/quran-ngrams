ab7f4722e2e5b26da0a5d8fe04344c2ab79993f2
Saqib <docbook.xml@gmail.com>
2016-06-22 06:34:56 +0000

    Adding hapax legomenon, dis legomenon, tris legomenon, and tetrakis legomenon implementation in Scala

--- target/scala-2.11/twirl/main/views/html/legomenon.template.scala
+++ target/scala-2.11/twirl/main/views/html/legomenon.template.scala
@@ -34,15 +34,14 @@ Seq[Any](format.raw/*1.65*/("""
             aria-label="Star saqib-ali/quran-ngrams on GitHub">Star</a>
         </p>
 
-        <h1>Quran N-gram Analytics</h1>
+        <h1>Hapax legomenon, dis legomenon, tris legomenon, and tetrakis legomenon in Quran</h1>
         <p>This Scala app lets you perform n-gram Analytics on the English Translation of Quran</p>
 
 
-        <a type="button" class="btn btn-lg btn-default" href="/?numOfWords=10"></span>10 words n-grams</a>
-        <a type="button" class="btn btn-lg btn-default" href="/?numOfWords=11"></span>11 words n-grams</a>
-        <a type="button" class="btn btn-lg btn-default" href="/?numOfWords=12"></span>12 words n-grams</a>
-        <a type="button" class="btn btn-lg btn-default" href="/?numOfWords=13"></span>13 words n-grams</a>
-        <a type="button" class="btn btn-lg btn-default" href="/?numOfWords=14"></span>14 words n-grams</a>
+        <a type="button" class="btn btn-lg btn-default" href="/?legomenon=1"></span>hapax legomenon</a>
+        <a type="button" class="btn btn-lg btn-default" href="/?legomenon=2"></span>dis legomenon</a>
+        <a type="button" class="btn btn-lg btn-default" href="/?legomenon=3"></span>tris legomenon</a>
+        <a type="button" class="btn btn-lg btn-default" href="/?legomenon=4"></span>tetrakis legomenon</a>
 
     </div>
 
@@ -51,15 +50,15 @@ Seq[Any](format.raw/*1.65*/("""
     <table class="table table-hover">
         <thead>
             <tr>
-                <th>N-gram</th>
-                <th>Repeated</th>
+                <th>Legomenon</th>
+                <th>Verses</th>
             </tr>
         </thead>
-        """),_display_(/*34.10*/for((word, verses) <- repeated) yield /*34.41*/ {_display_(Seq[Any](format.raw/*34.43*/(""" """),format.raw/*34.44*/("""<tr>
-            <td>"""),_display_(/*35.18*/word),format.raw/*35.22*/("""</td>
-            <td>"""),_display_(/*36.18*/for(List(chapter, versenum, word, versetxt, city) <- verses) yield /*36.78*/ {_display_(Seq[Any](format.raw/*36.80*/(""" """),_display_(/*36.82*/versetxt),format.raw/*36.90*/(""" """),format.raw/*36.91*/("""<strong>("""),_display_(/*36.101*/chapter),format.raw/*36.108*/(""" """),format.raw/*36.109*/(""": """),_display_(/*36.112*/versenum),format.raw/*36.120*/(""" """),format.raw/*36.121*/("""- """),_display_(/*36.124*/city),format.raw/*36.128*/(""")</strong><br/><br/> """)))}),format.raw/*36.150*/("""</td>            </tr>
-            """)))}),format.raw/*37.14*/("""
-    """),format.raw/*38.5*/("""</table>
+        """),_display_(/*33.10*/for((word, verses) <- repeated) yield /*33.41*/ {_display_(Seq[Any](format.raw/*33.43*/(""" """),format.raw/*33.44*/("""<tr>
+            <td>"""),_display_(/*34.18*/word),format.raw/*34.22*/("""</td>
+            <td>"""),_display_(/*35.18*/for(List(chapter, versenum, word, versetxt, city) <- verses) yield /*35.78*/ {_display_(Seq[Any](format.raw/*35.80*/(""" """),_display_(/*35.82*/versetxt),format.raw/*35.90*/(""" """),format.raw/*35.91*/("""<strong>("""),_display_(/*35.101*/chapter),format.raw/*35.108*/(""" """),format.raw/*35.109*/(""": """),_display_(/*35.112*/versenum),format.raw/*35.120*/(""" """),format.raw/*35.121*/("""- """),_display_(/*35.124*/city),format.raw/*35.128*/(""")</strong><br/><br/> """)))}),format.raw/*35.150*/("""</td>            </tr>
+            """)))}),format.raw/*36.14*/("""
+    """),format.raw/*37.5*/("""</table>
 </div>
 
 """)))}))
@@ -82,11 +81,11 @@ Seq[Any](format.raw/*1.65*/("""
 object legomenon extends legomenon_Scope0.legomenon
               /*
                   -- GENERATED --
-                  DATE: Wed Jun 22 06:27:02 UTC 2016
+                  DATE: Wed Jun 22 06:34:23 UTC 2016
                   SOURCE: /home/ubuntu/workspace/app/views/legomenon.scala.html
-                  HASH: 66d305ac6d1c99248b493e9281eb4d651650ff3a
-                  MATRIX: 579->1|737->64|766->68|838->132|877->134|905->136|2284->1488|2331->1519|2371->1521|2400->1522|2449->1544|2474->1548|2524->1571|2600->1631|2640->1633|2669->1635|2698->1643|2727->1644|2765->1654|2794->1661|2824->1662|2855->1665|2885->1673|2915->1674|2946->1677|2972->1681|3026->1703|3093->1739|3125->1744
-                  LINES: 20->1|25->1|27->3|27->3|27->3|29->5|58->34|58->34|58->34|58->34|59->35|59->35|60->36|60->36|60->36|60->36|60->36|60->36|60->36|60->36|60->36|60->36|60->36|60->36|60->36|60->36|60->36|61->37|62->38
+                  HASH: db289c7e2a3db7514d844a3d9389480c1da36735
+                  MATRIX: 579->1|737->64|766->68|838->132|877->134|905->136|2223->1427|2270->1458|2310->1460|2339->1461|2388->1483|2413->1487|2463->1510|2539->1570|2579->1572|2608->1574|2637->1582|2666->1583|2704->1593|2733->1600|2763->1601|2794->1604|2824->1612|2854->1613|2885->1616|2911->1620|2965->1642|3032->1678|3064->1683
+                  LINES: 20->1|25->1|27->3|27->3|27->3|29->5|57->33|57->33|57->33|57->33|58->34|58->34|59->35|59->35|59->35|59->35|59->35|59->35|59->35|59->35|59->35|59->35|59->35|59->35|59->35|59->35|59->35|60->36|61->37
                   -- GENERATED --
               */
           
