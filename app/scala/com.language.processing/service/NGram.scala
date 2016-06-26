70f9fd6c1b20296300a19fca16bde6425e06c09f
Saqib <docbook.xml@gmail.com>
2016-06-26 00:39:16 +0000

    Restructuring the project

--- app/scala/com.language.processing/service/NGram.scala
+++ app/scala/com.language.processing/service/NGram.scala
@@ -24,7 +24,7 @@ object NGram {
   
   
   def longestNGram(signs: List[String]): List[(String, Int)] = {
-      val all = ((5 to 24).foldRight(List[(String, Int)]())((i, l) => l ::: generateNGram(signs, i)))
+      val all = ((10 to 24).foldRight(List[(String, Int)]())((i, l) => l ::: generateNGram(signs, i)))
       .sortWith(_._1.length > _._1.length)
      
       all.map(calc(_, all))
