a40cd2887346852e89308f73e2a625aac881284d
Saqib <docbook.xml@gmail.com>
2016-07-03 02:07:04 +0000

    Restructuring the project

--- app/scala/com.language.processing/service/NGram.scala
+++ app/scala/com.language.processing/service/NGram.scala
@@ -42,15 +42,21 @@ object NGram {
     .setMaster(System.getenv("spark_cluster"))
     .setAppName("Simple Application")
     val sc = new SparkContext(conf)
-    sc.stop
     
       val all = ((8 to 24).foldRight(List[(String, Int)]())((i, l) => l ::: generateNGram(signs, i)))
       .sortWith(_._1.length > _._1.length)
      
-      all.map(calc(_, all))
+     
+     val distData = sc.parallelize(all)
+
+     val ngrams = distData.map(calc(_, all))
       .distinct
       .sortBy(_._2)
-      .reverse 
+   //   .reverse 
+      
+    sc.stop
+    ngrams.collect.toList
+
   }
      
   
