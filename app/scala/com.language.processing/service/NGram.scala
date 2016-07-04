1fbec708efed6c53ddc91f6abb19d9ba438dac8f
Saqib <docbook.xml@gmail.com>
2016-07-04 21:36:04 +0000

    Restructuring the project

--- app/scala/com.language.processing/service/NGram.scala
+++ app/scala/com.language.processing/service/NGram.scala
@@ -16,12 +16,6 @@ import scala.concurrent.duration._
 object NGram {
   
     def generateNGram(signs: List[String], numOfWords: Int): List[(String, Int)] = { // Scala N-gram secret sauce 
-    
- 
-
-    
-   // println(sc.toString);
-    
     (for( i <- 0 to signs.length-1) yield  signs(i)
       .replaceAll("([\\p{P}&&[^()]]+\\s*)+$", "")
       .replaceAll("([\\p{P}&&[^()]]+\\s*)+$", "")
@@ -29,20 +23,31 @@ object NGram {
       .sliding(numOfWords)
       .filter(_.size==numOfWords)
       .map(_.mkString(" "))
-      //.map(_.replaceAll("[\\p{P}\\s]+$", ""))
-      //.map(_.replaceAll("[\\p{P}\\s]+$", ""))
-
-
-    )
+   )
        .flatten
        .groupBy(x => x)   
        .toList   
        .map{case(ngram, occurrences) => (ngram, occurrences.length)}
        .filter{case(ngram, occurrences) => occurrences > 1}
-  }
+    }
   
   
-  
+    def generateNGramFuture(signs: List[String], numOfWords: Int): Future[List[(String, Int)]] = Future{ // Scala N-gram secret sauce 
+    (for( i <- 0 to signs.length-1) yield  signs(i)
+      .replaceAll("([\\p{P}&&[^()]]+\\s*)+$", "")
+      .replaceAll("([\\p{P}&&[^()]]+\\s*)+$", "")
+      .split(" ")
+      .sliding(numOfWords)
+      .filter(_.size==numOfWords)
+      .map(_.mkString(" "))
+   )
+       .flatten
+       .groupBy(x => x)   
+       .toList   
+       .map{case(ngram, occurrences) => (ngram, occurrences.length)}
+       .filter{case(ngram, occurrences) => occurrences > 1}
+    }
+    
   
   
 
@@ -59,11 +64,26 @@ object NGram {
   def longestNGram(signs: List[String]): List[(String, Int)] = {
     //  val all = time {((8 to 24).foldRight(List[(String, Int)]())((i, l) => l ::: generateNGram(signs, i))).sortWith(_._1.length > _._1.length)}
       val all = time {((6 to 24).map(i => generateNGram(signs, i)).reduce(_ ::: _)).sortWith(_._1.length > _._1.length)}
-      time {all
-      .map(calc(_, all))
+      val all2 = (6 to 24).map(i => generateNGramFuture(signs, i))
+      var all3 = Future.reduce(all2)(_ ::: _)
+    
+    
+    val all4 = Await.result(all3, Duration.Inf)
+    val all5 = all4.sortWith(_._1.length > _._1.length)
+    /*
+      all3.onComplete {
+	        case Success(result) => val returnval = result
+        	case Failure(exception) => val returnval = Nil
+        	case _ => val returnval = Nil
+
+        }*/
+
+      time {all5
+      .map(calc(_, all5))
       .distinct
       .sortBy(_._2)
       .reverse} 
+      
   }
      
   
