fca0634a87654ce397c16429277c655eeab2f3c9
Saqib <docbook.xml@gmail.com>
2017-12-21 03:27:57 +0000

    Restructuring the project

--- app/scala/com.language.processing/service/NGram.scala
+++ app/scala/com.language.processing/service/NGram.scala
@@ -66,7 +66,7 @@ object NGram {
       time{
       //val all = ((6 to 24).foldRight(List[(String, Int)]())((i, l) => l ::: generateNGram(signs, i))).sortWith(_._1.length > _._1.length)
       //val all = ((6 to 24).map(i => generateNGram(signs, i)).reduce(_ ::: _)).sortWith(_._1.length > _._1.length)
-      val ngramfutures = (10 to 24).map(i => generateNGramFuture(signs, i)) // Calculate Ngrams using Scala Futures for paralllelization
+      val ngramfutures = (19 to 30).map(i => generateNGramFuture(signs, i)) // Calculate Ngrams using Scala Futures for paralllelization
       val fut = Future.reduce(ngramfutures)(_ ::: _) // Reduce the Futures
       val allresult = Await.result(fut, 20 seconds)    
       val all = allresult.sortWith(_._1.length > _._1.length)
